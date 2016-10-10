5f3653fefc42dcd8c1c4e03b5a631bff4252f48a
Renato J. F <renatojen@gmail.com>
2016-10-09 21:34:41 -0300

    Launching RC

--- classi.py
+++ classi.py
@@ -10,13 +10,19 @@
 #imports
 from flask import Flask, render_template, request
 from watson_developer_cloud import VisualRecognitionV3 as vr3, TextToSpeechV1 as ts1
-from os import environ, remove, listdir, getenv
+from os import remove, listdir, getenv
 from os.path import join, dirname
 from datetime import datetime
 from time import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from db import Database
+from yaml import safe_load, dump
+
+dt = str(datetime.now())[:-5].replace(' ', '_').replace(':', '').replace('.', '') #YYYY-mm-DD_HHMMSSS   
+vr = None
+ts = None
+db = None
 
 #cleans previosly generated .ogg files in resources dir
 def clean_speech_files():   
@@ -27,50 +33,50 @@ def clean_speech_files():
 def get_images():
    global db
    return db.view()
-      
-#
+   
 # Initialization
-#
+def init():
+   global vr,ts,db
+   cfg = safe_load(open("cfg.yml"))
 
-dt = str(datetime.now())[:-5].replace(' ', '_').replace(':', '').replace('.', '') #YYYY-mm-DD_HHMMSSS
+   #validates VisualRecognitionV3 api key
+   vr3_key = getenv("WATSON_VR_API_KEY", cfg['watson']['vr_api_key']) #if WATSON_VR_API_KEY is not set, try local cfg to get api key
+      print("ERROR: Missing api key for Watson's VisualRecognitionV3")
+      quit()
+   
+   #validates TextToSpeechV1 username and password
+   ts1_user = getenv("WATSON_TS_USER", cfg['watson']['ts_user'])
+   ts1_pswd = getenv("WATSON_TS_PSWD", cfg['watson']['ts_pswd'])
+      print("ERROR: Missing credentials for Watson's TextToSpeechV1")
+      quit()
+   
+   db_url = getenv("CLASSIPY_DB_URL", cfg['db']['url'])
+      print("ERROR: Missing Database URL")
+      quit()
+      
+   #instances a VisualRecognitionV3 object using the api key
+   vr = vr3(api_key=vr3_key, version='2016-05-20')
 
-#checks for VisualRecognitionV3 api key
-vr3_key = environ.get("WATSON_VR_API_KEY")
-if vr3_key == None:      
-   print("ERROR: Missing api key for Watson's VisualRecognitionV3")
-   quit()
+   #instances a TextToSpeechV1 object using the credentials     
+   ts = ts1(username=ts1_user, password=ts1_pswd)
 
-#checks for TextToSpeechV1 username and password
-ts1_user = environ.get("WATSON_TS_USER")
-ts1_pswd = environ.get("WATSON_TS_PSWD")
-if ts1_user == None or ts1_pswd == None:      
-   print("ERROR: Missing credentials for Watson's TextToSpeechV1")
-   quit()
+   #initialize database
+   mrows = cfg['db']['max_rows'] if cfg['db']['max_rows'] != None else 0
+   tsize = cfg['db']['tbl_size'] if cfg['db']['tbl_size'] != None else 0
+   db = Database(db_url,  mrows,  tsize)
 
-#checks for ElephantSQL database URL
-url = environ.get("CLASSIPY_DB_URL")
-if url == None:      
-   print("ERROR: Missing Database URL")
-   quit()
+   #creates a scheduler to clean the resources every n, avoiding server overload
+   scheduler = BackgroundScheduler()
+   scheduler.start()
+   scheduler.add_job(
+       func=clean_speech_files,
+       trigger=IntervalTrigger(seconds=int(cfg['app']['clean_speech'])),
+       id='speech_job',
+       name='Speech Cleaner',
+       replace_existing=True)
+    
 
-#instances a VisualRecognitionV3 object using the api key      
-vr = vr3(api_key=vr3_key, version='2016-05-20')
-
-#instances a TextToSpeechV1 object using the credentials     
-ts = ts1(username=ts1_user, password=ts1_pswd)
-
-#instances database
-db = Database(url)
-
-#creates a scheduler to clean the resources every 30s, avoiding server overload
-scheduler = BackgroundScheduler()
-scheduler.start()
-scheduler.add_job(
-    func=clean_speech_files,
-    trigger=IntervalTrigger(seconds=30),
-    id='speech_job',
-    name='Speech cleaner',
-    replace_existing=True)
+init()
     
 #
 # Flask routines
@@ -86,15 +92,11 @@ def index():
          audio_file.write(ts.synthesize("Input an image URL and click the classi pie button!", accept='audio/ogg;codecs=opus', voice="en-US_AllisonVoice"))
          
    return render_template("index.html", label="", img="", audio_url="intro.ogg", db_data=get_images())
-
-@application.route('/about')
-def about():
-   return render_template("about.html")
-   
-#classify image, display image and classification results, generate results audio and save image data to database
+  
+#classify image, display image and classification results, generate audio results and save image data to database
 @application.route('/', methods=['POST'])
 def classify():
-   global dt ,vr,ts,db
+   global dt,vr,ts,db
    #updates dt variable (date and time)   
    dt = str(datetime.now())[:-5].replace(' ', '_').replace(':', '').replace('.', '')
    aurl = "output" + dt + ".ogg"      
@@ -105,6 +107,7 @@ def classify():
       img_url = str(request.form['img_url'])
       #Validation of image file
       if img_url.endswith(".jpg") or img_url.endswith(".png"):         
+         #calls VisualRecognitionV3 to classify the image
          img_data = vr.classify(images_url=img_url)         
          try:
             for data in img_data['images'][0]['classifiers'][0]['classes']:            
@@ -113,11 +116,12 @@ def classify():
             #remove the last ', '
             atxt = atxt[:-2]                     
             prefix = "There is "
+            #inserts image data on database
             db.insert(img_url, atxt.replace(" chance the image is related to:", ""))
          except KeyError as err:
             atxt = '\n' + "Image not supported. Try classifying another image."
             img_url=""
-            print("ERROR: Invalid dictonary key: " + str(err))                     
+            print("ERROR: Invalid dictionary key: " + str(err))                     
       else:
          atxt = '\n' + "Invalid image URL"
          img_url=""         
@@ -129,8 +133,7 @@ def classify():
    return render_template('index.html', label='\n' + atxt.replace(', ', '\n'), img=img_url, audio_url=aurl, db_data=get_images())           
 
 if __name__=="__main__":
-   # Bind to PORT/HOST if defined, otherwise default to 5050/localhost.
+   # Bind to VC_APP PORT/HOST if defined, otherwise default to 5000/localhost.
    PORT = int(getenv('VCAP_APP_PORT', '5000'))
    HOST = str(getenv('VCAP_APP_HOST', 'localhost'))
    application.run(host=HOST, port=PORT)
-   #application.run(debug=True)
--- db.py
+++ db.py
@@ -8,7 +8,6 @@
 """
 
 import psycopg2
-#import yaml
 from urllib.parse import urlparse, uses_netloc
 
 uses_netloc.append("postgres")
@@ -16,43 +15,45 @@ uses_netloc.append("postgres")
 class Database:
    
    #constructor
-   #def __init__(self, cfg, db_url=""):
-   def __init__(self, db_url):
-      #self.cfg = yaml.safe_load(open("cfg.yml"))
-      #if db_url !="":      
+   def __init__(self, db_url, db_max_rows="10", db_tbl_size="10"):            
       self.url = urlparse(db_url)
-      #else:
-      #   self.url = urlparse(self.cfg.database['url'])         
       self.conn=psycopg2.connect(database=self.url.path[1:], user=self.url.username, password=self.url.password, host=self.url.hostname, port=self.url.port)
       self.cur=self.conn.cursor()
       self.cur.execute("CREATE TABLE IF NOT EXISTS images (id SERIAL PRIMARY KEY, url TEXT, stats TEXT)")
       self.conn.commit()
       self.conn.close()
    
-   #inserts a new row in images table. If there are more than n rows, deletes older rows until there are only n rows left
-   def insert(self, url, stats):
+   #inserts a new row in images table. If there are more than [self.max_rows] rows, deletes older rows until there are only [self.max_rows] rows left. If [self.max_rows] == 0 insert infinite rows.
+   def insert(self, image_url, stats):
       self.conn=psycopg2.connect(database=self.url.path[1:], user=self.url.username, password=self.url.password, host=self.url.hostname, port=self.url.port)      
       self.cur=self.conn.cursor()
-      self.cur.execute("INSERT INTO images(url,stats) SELECT %s, %s WHERE NOT EXISTS (SELECT url FROM images WHERE url=%s)", (url, stats, url))
-      self.cur.execute("SELECT COUNT(*) OVER (), i.id FROM images i")
-      row=self.cur.fetchone()
-      i = int(row[0]) 
-      if i > 10:
-         self.cur.execute("DELETE FROM images WHERE id = any (array(SELECT id FROM images ORDER BY id LIMIT %s))", (i-10,))
+      self.cur.execute("INSERT INTO images(url,stats) SELECT %s, %s WHERE NOT EXISTS (SELECT url FROM images WHERE url=%s)", (image_url, stats, image_url))
+      if self.max_rows != 0:
+         self.cur.execute("SELECT COUNT(*) OVER (), i.id FROM images i")
+         row=self.cur.fetchone()
+         i = int(row[0]) 
+         if i > self.max_rows:
+            self.cur.execute("DELETE FROM images WHERE id = any (array(SELECT id FROM images ORDER BY id LIMIT %s))", (i-self.max_rows,))
+            
       self.conn.commit()
       self.conn.close()
    
-   #selects the last 10 rows
+   #selects the last [self.tbl_size] rows. If [self.tbl_size] == 0, selects all the rows.
    def view(self):
       self.conn=psycopg2.connect(database=self.url.path[1:], user=self.url.username, password=self.url.password, host=self.url.hostname, port=self.url.port)
       self.cur=self.conn.cursor()
-      self.cur.execute("SELECT * FROM images ORDER BY id DESC LIMIT 10")
+      if self.tbl_size != 0:
+         self.cur.execute("SELECT * FROM images ORDER BY id DESC LIMIT %s", (str(self.tbl_size),))
+      else:
+         self.cur.execute("SELECT * FROM images ORDER BY id DESC")
+         
       rows=self.cur.fetchall()
       self.conn.close()
       return rows
    
    #destructor
    def __del__(self):
+      #checks if connection is still open
       if self.conn.closed == 0:
          self.conn.close()
       
